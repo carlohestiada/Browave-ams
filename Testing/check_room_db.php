43c0a6c7733c5f4ad91e2e64d8ecfc8302178712
Clienth Carlo Hestiada <19.caloyhestiada@gmail.com>
2026-07-22 11:58:02 +0800

    update meal plan

--- Testing/check_room_db.php
+++
@@ -1,6 +0,0 @@
-﻿<?php
-$pdo = new PDO("mysql:host=127.0.0.1;dbname=browave_ams","root","");
-foreach (['accommodations','buildings','floors','rooms'] as $table) {
-    $count = $pdo->query("SELECT COUNT(*) FROM $table")->fetchColumn();
-    echo "$table: $count\n";
-}
--- Testing/schema_check.php
+++
@@ -1,5 +0,0 @@
-﻿<?php
-$pdo = new PDO("mysql:host=127.0.0.1;dbname=browave_ams","root","");
-foreach ($pdo->query("SHOW CREATE TABLE departments") as $row) {
-    echo $row['Create Table'] . "\n";
-}
