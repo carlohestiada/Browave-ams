de5d7e1cefcb7ef9c3c01b8784fd821e4f2a56f3
Clienth Carlo Hestiada <19.caloyhestiada@gmail.com>
2026-08-12 09:48:41 +0800

    Added Sidebar signout button

--- public/layouts/sidebar.php
+++ public/layouts/sidebar.php
@@ -5,6 +5,7 @@ $allowedPages = $allowedPages ?? (function_exists('getAllowedPagesForRole') ? ge
 
 ?>
 
+<link rel="stylesheet" href="assets/css/style.css?v=<?= filemtime(__DIR__ . '/assets/css/style.css') ?>">
 
 <div class="sidebar">
 
@@ -225,6 +226,14 @@ $allowedPages = $allowedPages ?? (function_exists('getAllowedPagesForRole') ? ge
             <?php endif; ?>
 
         </ul>
+
+    </div>
+    <!-- Sign Out -->
+    <div class="sidebar-signout-wrap">
+        <button type="button" class="sidebar-signout-btn" onclick="document.getElementById('logout-form') ? document.getElementById('logout-form').submit() : window.location.href='logout.php'">
+            <i class="bi bi-box-arrow-right nav-icon"></i>
+            <span>Sign Out</span>
+        </button>
     </div>
 </div>
 </div>
